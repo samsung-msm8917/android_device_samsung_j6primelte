ff0f86b1a9743e9e73bbc304de40a1b8cba8943c
BarryBlackCat <everydayonechoice@gmail.com>
2021-05-01 17:07:35 +0000

    j6primelte: Update to redfin april FP

--- fluid_j6primelte.mk
+++ fluid_j6primelte.mk
@@ -45,7 +45,7 @@ TARGET_VENDOR_PRODUCT_NAME := j6primelte
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="j6primelteub-user 10 QP1A.190711.020 J610GUBU4CTI1 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1D.210105.003/7005430:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RQ1D.210105.003/7005430:user/release-keys
+    ro.build.fingerprint=google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
